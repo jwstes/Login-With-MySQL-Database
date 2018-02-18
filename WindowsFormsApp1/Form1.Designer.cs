de9c11a6caef54eba3033e979d2bc44744e9f627
jwstes <jwstes@gmail.com>
2018-02-18 23:55:21 +0800

    Project Files

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_register = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(16, 12);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(140, 20);
+            this.txt_user.TabIndex = 1;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(16, 46);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(140, 20);
+            this.txt_pass.TabIndex = 3;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(162, 12);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 4;
+            // 
+            // btn_register
+            // 
+            this.btn_register.Location = new System.Drawing.Point(162, 46);
+            this.btn_register.Name = "btn_register";
+            this.btn_register.Size = new System.Drawing.Size(75, 23);
+            this.btn_register.TabIndex = 5;
+            this.btn_register.Text = "Register";
+            this.btn_register.UseVisualStyleBackColor = true;
+            this.btn_register.Click += new System.EventHandler(this.btn_register_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 115);
+            this.Controls.Add(this.btn_register);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txt_user);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MySQL Login - Jwstes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_register;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection con;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public Form1()
+        {
+            server = "localhost";
+            database = "devlogin";
+
+            uid = "";
+            password = "";
+
+            string conString;
+            conString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+
+            con = new MySqlConnection(conString);
+
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string user = txt_user.Text;
+            string pass = txt_pass.Text;
+
+            if (IsLogin(user, pass))
+            {
+                label1.ForeColor = Color.Green;
+                label1.Text = $"Welcome {user}";
+            }
+            else
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = $"{user} doesn't exist";
+            }
+        }
+
+        private bool IsLogin(string user, string pass)
+        {
+            string query = $"SELECT * FROM users WHERE username='{user}' AND password='{pass}';";
+
+            try
+            {
+                if (OpenConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        reader.Close();
+                        con.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        con.Close();
+                        return false;
+                    }
+                }
+                else
+                {
+                    con.Close();
+                    return false;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                con.Close();
+                return false;
+            }
+        }
+
+        private bool CanRegister(string user, string pass)
+        {
+            string query = $"INSERT INTO `users` (`id`, `username`, `password`) VALUES (NULL, '{user}', '{pass}');";
+            string checkuserquery = $"SELECT * FROM users WHERE username='{user}' AND password='{pass}';";
+
+            try
+            {
+                if (OpenConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    reader.Close();
+
+                    MySqlCommand cmdcheck = new MySqlCommand(checkuserquery, con);
+                    MySqlDataReader readercheck = cmdcheck.ExecuteReader();
+
+                    if (readercheck.Read())
+                    {
+                        con.Close();
+                        readercheck.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        con.Close();
+                        readercheck.Close();
+                        return false;
+                    }
+                }
+
+                else
+                {
+                    con.Close();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                return false;
+            }
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                con.Open();
+                return true;
+            }
+
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        label1.ForeColor = Color.Red;
+                        label1.Text = $"Connection to server Failed";
+                        break;
+
+                    case 1:
+                        label1.ForeColor = Color.Red;
+                        label1.Text = $"Incorrect username or password for SQL Database";
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private void btn_register_Click(object sender, EventArgs e)
+        {
+            string user = txt_user.Text;
+            string pass = txt_pass.Text;
+
+            if (CanRegister(user, pass) == true)
+            {
+                label1.ForeColor = Color.Green;
+                label1.Text = $"{user} registed";
+            }
+            else
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = $"{user} not registered";
+            }
+        }
+    }
+}
